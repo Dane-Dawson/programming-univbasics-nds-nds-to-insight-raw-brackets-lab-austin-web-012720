9c63811c2cb8a9a97103ee15014064a1d1c8c2f9
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-16 04:49:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,16 +18,6 @@ def directors_totals(nds)
    result[row_index] = "/'#{directors_database[row_index]}=>#{director_total}/'"
    row_index += 1
  end
-  #result = {
-  #}
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
+
 
 end
